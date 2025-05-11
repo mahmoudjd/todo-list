a3bd4b925be6d91faf217dcca7b8c763ff7a301e
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-11 14:39:56 +0200

    Update package.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,8 +22,8 @@ app.get("/tasks/:taskId", getTask);
 app.put("/tasks/:taskId", updateTask);
 app.delete("/tasks/:taskId", deleteTask);
 
-const PORT = process.env.PORT || 3000;
-const MONGO_URL = "mongodb://127.0.0.1:27017/tasks" || process.env.MONGO_URL;
+const PORT = process.env.PORT ?? 3000;
+const MONGO_URL = process.env.MONGO_URL ?? "mongodb://127.0.0.1:27017/tasks";
 
 mongoose.connect(MONGO_URL!).then(() => {
   console.log(`listening on ${PORT}`);
